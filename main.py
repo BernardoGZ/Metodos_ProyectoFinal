14194fad75045089c1d8bafae159f69892f708e7
BernardoG <A00570682@itesm.mx>
2022-11-08 21:05:10 -0600

    Added file and console lecture functions. Added data.txt with testing data

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ Print functions
 '''
 
 def print_initial(matrix, teams):
-    result = print_matrix(matrix) + '\n'
+    result = print_matrix(matrix) + '\n\n'
     result += print_teams(teams)
+    result += "-----------------------------------------------------------------------\n\nGame started!\n\n"
     return result
 
 def print_matrix(matrix):
@@ -47,7 +48,6 @@ Reconfiguring stochastic matrix
 '''
     General project functions
 '''
-
 
 def random_matrix(size = 3):
     #Initializing matrix and lists to be added
@@ -99,24 +99,77 @@ def choose_poor_guy_v2(team_number, main_matrix):
     num_attack = randint(0, 8)
     return probability_list[num_attack]
 
+def lectura_archivo(file_name):
+    groups = []
+    teams_quantity = 0
 
+    with open(file_name) as r:        
+        teams_quantity = int(r.readline().replace("\n", ''))
+        for i in range(teams_quantity):
+            groups.append(int(r.readline().replace("\n", '')))
+        data = r.readlines()
+    
+    #Cleaning data
+    matrix = np.zeros(shape=(teams_quantity, teams_quantity))
+    i = 0
+    j = 0
+
+    for x in data:
+        if j == teams_quantity:
+            j = 0
+            i += 1
+        x = x.replace("\n", '')
+        if(i == j and float(x) != 0.0):
+            print("Matrix must have zeroed main diagonal. Please check data!")
+            exit()
+        matrix[i][j] = (float(x))
+        j += 1
+    # print(teams_quantity, groups, matrix)
+    return teams_quantity, groups, matrix
+
+def lectura_consola():
+    groups = []
+    teams_quantity = 0
+
+    teams_quantity = int(input("Ingresa la cantidad de equipos: "))
+
+    for x in range(teams_quantity):
+        groups.append(int(input(f"Ingresa los elementos del Grupo {x+1}: ")))
+    
+    matrix = np.zeros(shape=(teams_quantity, teams_quantity))
+
+    for i in range(teams_quantity):
+        for j in range(teams_quantity):
+            matrix[i][j] = float(input(f"Ingresa el valor en posicion ({i+1}, {j+1}): "))
+            if (i == j and matrix[i][j] != 0.0):
+                print("Matrix must have zeroed main diagonal. Please check data!")
+                exit()
+
+    return teams_quantity, groups, matrix
 
 if __name__ == '__main__':
     #Variables iniciales pueden ser elegidas por el usuario
     
-    # sel = input("Deseas ingresar los valores iniciales por tu cuenta? Y/N")
-
-    teams_quantity = 5
-    max_warriors = 10    
-    sel = "N"
+    sel = input("Deseas ingresar los valores iniciales por tu cuenta? Y/N: ")   
+    # sel = "y"
 
     if (sel == "N" or sel == "n"):
-        # teams_quantity = int(input("Ingresa la cantidad de equipos: "))
-        # max_warriors = int(input("Ingresa la cantidad maxima de guerreros: "))
+        teams_quantity = randint(0, 10)
+        max_warriors = randint(0, 20)
         main_matrix = random_matrix(teams_quantity)
         teams = random_warriors(teams_quantity, max_warriors)
     else:
-        pass
+        resp = input("Deseas ingresar la matriz desde un archivo? Y/N: ")
+        # resp = 'n'
+        is_a_file = True if(resp == "Y" or resp == 'y') else False
+        if(is_a_file):
+            print("Recuerda: El archivo debe contener los datos separados en diferentes lineas")
+            file_name = input("Ingresa el nombre o PATH del archivo: ")
+            # file_name = "data.txt"
+            teams_quantity, teams, main_matrix = lectura_archivo(file_name)
+        else:
+            teams_quantity, teams, main_matrix = lectura_consola()
+            pass
 
     playing = True
 
